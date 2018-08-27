25ce64e04ebd5ca8ce4804cf99638b34794d0cf9
Raul Pereira <raul.pereira@gmail.com>
2018-08-27 16:19:49 -0300

    filter grid and build pixel map for the image struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,9 +1,11 @@
 defmodule Identicon do
-  def main (input) do
+  def main(input) do
     input
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
   end
 
   def hash_input(input) do
@@ -31,5 +33,29 @@ defmodule Identicon do
 
   def mirror_row([first, second | _tail] = row) do
     row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter grid, fn({code, _index}) ->
+        rem(code, 2) == 0
+      end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map grid, fn({_code, index}) ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 end
