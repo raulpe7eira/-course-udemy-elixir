4e987a288202f0744c741ccd7fae8202cf56f04b
Raul Pereira <raul.pereira@gmail.com>
2018-09-04 02:48:47 -0300

    Config heroku deploy

--- discuss/config/prod.exs
+++ discuss/config/prod.exs
@@ -13,8 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :discuss, Discuss.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "course-udemy-elixir--phoenix.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -62,15 +64,11 @@ config :logger, level: :info
 # import_config "prod.secret.exs"
 
 # Configure your database
-config :rsvp, Rsvp.Repo,
+config :discuss, Discuss.Repo,
     adapter: Ecto.Adapters.Postgres,
     url: System.get_env("DATABASE_URL"),
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     ssl: true
-
-# Configure you endpoint
-config :discuss, Discuss.Endpoint,
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your github oauth provider
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
--- discuss/web/channels/user_socket.ex
+++ discuss/web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Discuss.UserSocket do
   channel "comments:*", Discuss.CommentChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
